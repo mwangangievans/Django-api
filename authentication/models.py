9397effbea8e80c6ff9850f356e1acb1dd6c220d
emwangangi <emwangangi@jambopay.com>
2024-03-23 18:02:37 +0300

    first commit

---
+++ authentication/models.py
@@ -0,0 +1,112 @@
+from django.utils import timezone
+from django.db import models
+from authentication import apps
+from helpers.models import TrackingModel
+from django.contrib.auth.models import (PermissionsMixin, UserManager ,AbstractBaseUser)
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.hashers import make_password
+
+
+
+
+# Create your models here.
+#add new properties access_token , is_email_verified
+#Use email and password instead of userbname/password
+
+class myUserManager(UserManager):
+     def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+        
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        # GlobalUserModel = apps.get_model(
+        #     self.model._meta.app_label, self.model._meta.object_name
+        # )
+        username = self.model.normalize_username(username)
+        # username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+     def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+     def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser,PermissionsMixin, TrackingModel):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Username and password are required. Other fields are optional.
+    """
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        ("username"),
+        max_length=150,
+        unique=True,
+        help_text=(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": ("A user with that username already exists."),
+        },
+    )
+    
+    email = models.EmailField(("email address"), blank=False , unique=True)
+    is_staff = models.BooleanField(
+        ("staff status"),
+        default=False,
+        help_text=("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        ("active"),
+        default=True,
+        help_text=(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(("date joined"), default=timezone.now)
+    email_verified = models.BooleanField(
+        ("active"),
+        default=False,
+        help_text=(
+            "Designates whether this users email is verified."
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    objects = myUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    @property
+    def token():
+        return ""
+        
---
+++ authentication/tests/test_models.py
@@ -0,0 +1,35 @@
+from rest_framework.test import APITestCase
+from authentication.models import User
+
+class TestModel(APITestCase):
+
+    def test_create_user(self):
+      user = User.objects.create_user('evans','evans@gmail.com','1234')
+      self.assertIsInstance(user,User)
+      self.assertFalse(user.is_staff)
+      self.assertFalse(user.is_superuser)
+      self.assertEqual(user.email ,'evans@gmail.com')
+
+
+    def test_create_super_user(self): 
+      user = User.objects.create_superuser('evans','evans@gmail.com','1234')
+      self.assertIsInstance(user,User)
+      self.assertTrue(user.is_staff)
+      self.assertTrue(user.is_superuser)
+      self.assertEqual(user.email ,'evans@gmail.com')
+        
+
+    def test_raises_error_with_message_when_no_email_supplied(self):
+       with self.assertRaisesMessage(ValueError,'The given email must be set'):
+          User.objects.create_user(username='username',email='',password='1234')
+
+
+    def test_cant_create_super_user_with_no_is_staff_status(self):
+       with self.assertRaisesMessage(ValueError,'Super user must have is_staff=True.'):
+          User.objects.create_superuser(username='evans',email='evan@gmail.com',password='1234',is_staff=True)
+
+    def test_cant_create_super_user_with_no_is_super_user_status(self):
+       with self.assertRaisesMessage(ValueError,'Super user must have is_superuser=True.'):
+          User.objects.create_superuser(username='evans',email='evan@gmail.com',password='1234',is_superuser=True)
+
+          
